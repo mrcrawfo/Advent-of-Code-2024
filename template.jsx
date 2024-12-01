29bb0b047e898a08fccf566cb6980fe3af40b11b
Max Crawford <mrcrawfo@gmail.com>
2024-12-01 03:19:30 -0500

    feat: Updated .gitignore and added template file

---
+++ template.jsx
@@ -0,0 +1,29 @@
+const demoInput = [
+
+];
+
+const testInput = [
+
+];
+
+const test1 = (input) => {
+
+};
+
+const test2 = (input) => {
+
+};
+
+console.log('Answer - Part 1 - Input 1');
+console.log(test1(demoInput));
+// 
+//console.log('Answer - Part 1 - Input 2');
+//console.log(test1(testInput));
+// 
+
+//console.log('Answer - Part 2 - Input 1');
+//console.log(test2(demoInput));
+// 
+//console.log('Answer - Part 2 - Input 2');
+//console.log(test2(testInput));
+// 
